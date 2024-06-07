2b734c150dbcd7f0e55e7981f6d5848b07a09fed
santiagodev <sanxp18@gmail.com>
2024-06-06 21:09:21 -0400

    logic to submit the rating and change the card

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 //Manipulando el DOM
 const inputList = document.querySelectorAll(".input__fields");
 const containerOfInput = document.querySelectorAll(".card__input-container");
+const form = document.querySelector("form");
 const submitButtom = document.getElementById("button-submit");
+const firstCard = document.getElementById("rating-component");
+const secondCard = document.getElementById("thank-you-component");
+const ratingNumber = document.querySelector(".rating-number");
 
 console.log(inputList, submitButtom);
+
+//evitar que el formulario se recargue cuando se haga click en submit
 
 //Primero recorrer los inputs
 inputList.forEach((checkbox, index) => {
@@ -13,15 +19,29 @@ inputList.forEach((checkbox, index) => {
             if (otherCheckbox !== checkbox) {
               otherCheckbox.disabled = checkbox.checked;
             }
-            //cambiar color del container
-            if(checkbox.checked) {
-                containerOfInput[index].style.backgroundColor = "grey";
-            } else {
-                containerOfInput[index].style.backgroundColor = "";
-            }
-    
-            console.log("presiono el input");
 
+            console.log("presiono el input");
         })
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            firstCard.classList.add("interactive-card--hidden");
+            secondCard.classList.remove("interactive-card--hidden");
+            ratingNumber.innerHTML = inputList[index].value;
+            //cambiar color del container
+            // if(checkbox.checked) {
+            //     firstCard.classList.add("interactive-card--hidden");
+            //     secondCard.classList.remove("interactive-card--hidden");
+            //     ratingNumber.innerHTML = inputList[index].value;
+            
+        })
+
+        if(checkbox.checked) {
+            containerOfInput[index].style.backgroundColor = "grey";
+            
+        } else {
+            containerOfInput[index].style.backgroundColor = "";
+        }
+        
     });
 })
